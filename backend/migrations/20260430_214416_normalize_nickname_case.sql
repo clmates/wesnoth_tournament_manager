f29dbf7e15ecf5be4c57e76c4e9b26b5049e1e8f
clmates <clmates@gmail.com>
2026-04-30 22:04:24 +0200

    Remove data migration file - to be executed manually in each environment

--- backend/migrations/20260430_214416_normalize_nickname_case.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Normalize user nickname case to match forum username
--- Date: 2026-04-30 21:44:16
--- Description: Update existing nicknames from lowercase to original forum case from phpbb3_users.username
-
-UPDATE tournament.users_extension ue
-JOIN forum.phpbb3_users u ON LOWER(ue.nickname) = LOWER(u.username)
-SET ue.nickname = u.username
-WHERE ue.nickname != u.username;
